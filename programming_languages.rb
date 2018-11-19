62e3b80528ab8e3ac3d37f7f8943ab98a2926bf5
capable-publisher-7225 <marcusplenty@gmail.com>
2018-11-19 02:24:45 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,5 +1,10 @@
 def reformat_languages(languages)
   new_hash={}
   languages.each do |k1,v1|
-  end 
+    k1.each do |k2,v2|
+      k2.each do |k3,k3|
+        new_hash[k2] = {:type => v3, :style => k1}
+      end 
+    end
+  end
 end
